0b988056020751dbfc05044e0d5e78057503ebe1
Jire-khadrey <shitturasheed699@gmail.com>
2022-10-11 07:51:41 +0100

    All in

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 07:39:54 2022
+@author: Shittu Rasheed
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
