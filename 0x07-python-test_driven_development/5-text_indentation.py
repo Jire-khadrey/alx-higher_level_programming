27c393b41ed69b1939d3ed794521e786f62b5837
Jire-khadrey <shitturasheed699@gmail.com>
2022-10-16 08:32:43 +0100

    Tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 07:43:34 2022
+@author: Shittu Rasheed
+"""
+
+
+def text_indentation(text):
+    """
+    prints a string with 2 new lines after '.', '?', and ':'
+    Args:
+        text (int): text to print
+    Raises:
+        TypeError: "text must be a string"
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    string = text.replace('.', '.\n\n').replace(':', ':\n\n')\
+        .replace('?', '?\n\n')
+    for i in range(len(text)):
+        string = string.replace('\n ', '\n')
+
+    print(string, end='')
