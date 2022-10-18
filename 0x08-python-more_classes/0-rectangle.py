4c7071b2adf6f93c4386dc6da22a5da217c8abcf
Jire-khadrey <shitturasheed699@gmail.com>
2022-10-18 06:23:34 +0100

    All new on OOP

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 2022
+@author: Shittu Rasheed
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
