a0a12f6aaee501d18af1aeed16f44a0091e5701c
Jire-khadrey <shitturasheed699@gmail.com>
2022-10-25 12:35:20 +0100

    once

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 11:13:37 2022
+@author: Shittu rasheed
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a json string to a python object
+    Arguments:
+        my_obj (str): The inputed object to convert in json format
+    Return:
+        A Python object from json
+    """
+    return json.loads(my_str)
